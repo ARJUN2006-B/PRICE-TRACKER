46b34a97d80767cdb9d618af05f23c6ebd85a98a
AkileshwaranN <akileshwaran104@gmail.com>
2025-10-08 19:42:49 +0530

    removed all files from repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // 1. Must import the plugin
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // 2. Must call the plugin to enable JSX processing
+});
