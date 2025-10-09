0db4836a224886f1bdc83116096231b7cfa33d6a
dineshpranavks <dineshpranav.ks@gmail.com>
2025-10-09 16:04:31 +0530

    Remove all project files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // 1. Must import the plugin
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], // 2. Must call the plugin to enable JSX processing
-});
