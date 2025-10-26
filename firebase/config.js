73cde659887c35ad7627977c0635a505a69aae35
dineshpranavks <dineshpranav.ks@gmail.com>
2025-10-26 12:29:23 +0530

    sign in logic added in firebase

--- firebase/config.js
+++ firebase/config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCQCNF4VysLczP6DropCtDRv_UtgVbacLU",
---
+++ signup.js
@@ -0,0 +1,22 @@
+import { auth } from "./firebase/config.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+document.getElementById("signupBtn").addEventListener("click", async (e) => {
+  e.preventDefault();
+  const email = document.getElementById("signup-email").value;
+  const password = document.getElementById("signup-password").value;
+  const confirmPassword = document.getElementById("signup-confirm-password").value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("Signup successful!");
+    window.location.href = "Login.html"; // redirect after signup
+  } catch (error) {
+    alert(error.message);
+  }
+});
